abeb1e5bee5b1b8f248aeae0191e07edf58bea10
Sainz <80474991+sainz1407@users.noreply.github.com>
2025-05-16 19:39:46 +0700

    Update scrape_module.py

--- scrape_module.py
+++ scrape_module.py
@@ -1,54 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-import re
 
 DOMAIN = "https://sakurazaka46.com"
 
-def scrape_images(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0',
-        'Referer': DOMAIN
-    }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'html.parser')
+def scrape_images(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
     images = []
-
-    path_pattern = re.compile(
-    r'^/(files|images)/14/(?!.*(app/|common/pages/app_guide/images|s46/img|artist/|prof/)).*\.(?:jpe?g)$',
-    re.IGNORECASE
-    )
-    blog_prof_section = soup.select_one('div.blog-prof-r')
-    excluded_images = []
-    if blog_prof_section:
-        excluded_images = blog_prof_section.select('img[src]')
+    seen = set()
     for img in soup.select('img[src]'):
-        if img in excluded_images:
-            continue
         src = img['src']
-        if path_pattern.search(src):
-            clean_src = requests.utils.unquote(src)
-            if clean_src.startswith('/'):
-                clean_src = DOMAIN + clean_src
-            images.append(clean_src)
+        if src.startswith('/'):
+            src = DOMAIN + src
+        src = requests.utils.unquote(src)
+        if src not in seen:
+            seen.add(src)
+            images.append(src)
     return images
 
 def get_blog_posts():
-    api_url = f"{DOMAIN}/s/s46app/api/json/diary?cd=blog"
-    response = requests.get(api_url)
+    api_url = f"{DOMAIN}/s/s46app/api/json/diary?cd=blog&get=B"
+    headers = {'Accept-Encoding': 'gzip, deflate', 'User-Agent': 'Mozilla/5.0'}
+    params = {
+        'getnum': '3',  # Limit new post
+        'st': '0'
+    }
+    response = requests.get(api_url, headers=headers, params=params)
     data = response.json()
     
     posts = []
     for blog in data['blog']:
         clean_url = blog['link'].replace("s46app/", "s46/").split('?')[0]
         
+        html_content = blog.get('content', '')
+        post_images = []
+        if '<img' in html_content:
+            post_images = scrape_images(html_content)
+
         posts.append({
             'post_id': int(blog['id']),
             'url': clean_url,
             'member': blog['creator'],
             'title': blog['title'],
-            'date': blog['pubdate']
+            'date': blog['pubdate'],
+            'images': post_images
         })
     # excluded_ids = {59972}
     # posts = [p for p in posts if p['post_id'] not in excluded_ids]
